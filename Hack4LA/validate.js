464464c278da553b3007dc637b8cf87cb5b23d48
Akib Mohaimenur Rahman <akib.rhast@gmail.com>
2021-03-14 20:50:47 -0400

    code clean up

--- Hack4LA/validate.js
+++ Hack4LA/validate.js
@@ -1,60 +1,82 @@
-import { readdir, readFile, unlinkSync, readFileSync, appendFileSync } from 'fs';
+import {readFileSync, appendFileSync, readdirSync } from 'fs';
 import fm from 'front-matter';
 import { Validator } from 'jsonschema';
-const v = new Validator();
+const validator = new Validator();
 
 //["Active","On Hold","Completed"]
 const schemaOptions ={
-  "Active": "schema_active.json",
-  "On Hold": "schema_onHold.json",
-  "Completed": "schema_completed.json"
+  "Active"    : "./Hack4LA/schemas/schema_active.json",
+  "On Hold"   : "./Hack4LA/schemas/schema_onHold.json",
+  "Completed" : "./Hack4LA/schemas/schema_completed.json"
 }
 
-readdir('./Hack4LA/testFiles/',function(err,files){
-  if (err) {
-    throw err;
+const testFileLocation = './Hack4LA/testFiles';
+
+(function main() {
+  
+  const file_path_list = get_all_file_path(testFileLocation);
+
+  const report = gernerateReport(file_path_list);
+
+  const clean_report = cleanReport(report);
+
+  writeCleanReportToFile('./Hack4LA/report.json',clean_report);
+
+
+})();
+
+
+function gernerateReport(file_path_list){
+  const validation_list = [];
+  for(const file_path of file_path_list){
+    let file_data = readFileSync(file_path,'utf8');
+    let extracted_data = front_matter_extract(file_data);
+    let schema_to_use = JSON.parse(readFileSync(schemaOptions[extracted_data.attributes.status]));
+
+    validation_list.push( validator.validate(extracted_data.attributes, schema_to_use) );
+  }
+  return validation_list;
+}
+
+function cleanReport(orginal_report_list){
+  let cleaned_report = [];
+  for(const original_report of orginal_report_list){
+    if(original_report.errors.length > 0){
+      let arr = [];
+      for(let item of original_report.errors){
+        arr.push({"message":item.message,"stack":item.stack})
+      }
+      let obj = {'file':original_report.instance.title,'errors': arr}
+      cleaned_report.push(obj);
+    }
+
   }
 
-  files.forEach(function(file,index){
+  return cleaned_report;
+}
 
-    readFile(`./Hack4LA/testFiles/${file}`, 'utf8', function(err, data){
-      if (err) {
-        throw err;
-      }
-      try{
-        let {attributes} = fm(data)
-        let filePath = `./Hack4LA/${attributes.status}_validationResult.json`;
-        let schema = JSON.parse(readFileSync(`./Hack4LA/schemas/${schemaOptions[attributes.status]}`));
-        let x = v.validate(attributes, schema)
-        
-        if(x.errors.length > 0){
-          let arr = [];
-          for(let item of x.errors){
-            arr.push({"message":item.message,"stack":item.stack})
-          }
-          let obj = {'filename':file,'errors': arr}
-          appendFileSync(filePath, JSON.stringify(obj,null,4),() => {})
-        }
-        
-      }
-      catch(err){
-        console.log(file,err)
-      }
+function writeCleanReportToFile(filename,cleanReport){
+  appendFileSync(filename, JSON.stringify(cleanReport,null,4),() => {})
+}
 
+
+
+function front_matter_extract(file_data){
+  let extracted_data = fm(file_data);
+  return extracted_data;
+}
+
+function get_all_file_path(directory){
+  try {
+    const file_path_list = []
+    let files =readdirSync(directory)
+    files.forEach(file=>{
+      file_path_list.push(`${directory}/${file}`)
     })
-  })
+    return file_path_list;
+  } catch (error) {
+      throw new Error(error)
+  }
 
+}
 
-    
-})
-
-
-/**
- * Give a folder thats a jekyl collections and a json schema, generate a report on weather the front matter
- * of each file follows the constraint as specified in the json schema
- * 
- * @param {String} collection_dir - The path to a directory that's a jekyll collection
- * @param {String} schema - The path to a schema file to validate the collections front matter against
- * @return {JSON} - Returns a json object/Saves to file result of the validation
- *
- */
